1e28779f1c28d03f97d5ff5327a4a2bfc67829d3
rittik12 <guptaritik823@gmail.com>
2026-08-18 00:07:52 +0530

    Day 7

---
+++ Day-7/list.py
@@ -0,0 +1,4 @@
+names= ["Abhishek", "Rittik", "Fred"]
+names.append("Shubam")
+names.remove("Abhishek")
+print(names[0:2])
---
+++ Day-7/tuple.py
@@ -0,0 +1,3 @@
+s3_bucket_list=("demo_s3", "test_s3")
+s3_bucket_list.add("new_s3")
+print(s3_bucket_list)
