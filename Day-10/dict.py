ad1feb8dbf21be54cec0997fcec9292eb553f8de
rittik12 <guptaritik823@gmail.com>
2026-08-22 23:53:49 +0530

    github integration

---
+++ Day-10/dict.py
@@ -0,0 +1,14 @@
+students_details = [
+    {
+    "name": "Rittik",
+    "age": "26",
+    "company": "S&P"
+    },
+    {
+    "name": "Mohak",
+    "age": "26",
+    "company": "Nagarro"
+    }
+]
+
+print(students_details[1]["company"])
